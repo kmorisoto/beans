537ad62730c6574eb915fb5f676cb41a9246817e
morisoto <k.morisoto@quo-digital.jp>
2022-11-05 17:02:08 +0900

    Add hello world

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {Root} from "./routes/root";
+import {ErrorPage} from "./error-page";
+import {Hello} from "./routes/hello";
+import {HelloJp} from "./routes/hello.jp";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root/>,
+        errorElement: <ErrorPage/>,
+        children: [
+            {
+                path: "hello",
+                element: <Hello/>,
+            },
+            {
+                path: "hello-jp",
+                element: <HelloJp/>,
+            },
+        ]
+    },
+])
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/routes/hello.jp.tsx
@@ -0,0 +1,5 @@
+export const HelloJp = () => {
+    return (
+        <div>こんにちは世界!</div>
+    );
+}
